df790be7da5ba515ace09ef3e5c4568a4d5c7262
frank-feulner <frank.feulner@aexea.de>
2016-04-18 16:42:48 +0200

    rebuilding site Mo 18 Apr 2016 16:42:48 CEST

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 #aktuellen Pfad abfragen
   cd $(dirname $0)
   currentpath=$(PWD)
-
+  cd ..
 #aus aktuellem Pfad den Pfad des Jupyter Notebooks ableiten und dorthin wechseln
   contentpath=${currentpath/scripts/Lessons}/
   publishpath=${currentpath/scripts/content}/
